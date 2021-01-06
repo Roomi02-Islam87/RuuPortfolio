40a6e85377746e4ced9ef367f09d8401b3dcdede
Roomi <61022537+Roomi02-Islam87@users.noreply.github.com>
2021-01-06 21:06:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import MyNavbar from "./components/my-Navbar/mynavbar.component";
+
+const App= () => {
+  return (
+    <div>
+      <MyNavbar/>My portfolio
+          </div>
+  );
+}
+
+export default App;
