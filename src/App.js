744a4a09dfb8b4164e04b7c8164fdd8b0d2fdcae
roomi islam <roomiislam098@gmail.com>
2021-01-07 19:25:20 +0530

    skills

--- src/App.js
+++ src/App.js
@@ -3,16 +3,60 @@ import './App.css';
 import MyNavbar from "./components/my-Navbar/mynavbar.component";
 import MyCarausel from "./components/my-carausel/my-carausel.component"
 import TitleMessage from "./components/title-message/title-message.component"
+import About from "./pages/about/about.component";
+import Skills from './pages/skills/skills.component';
+
+
+
+import Fade from "react-reveal/Fade";
+import { Parallax } from "react-parallax";
+import Container from 'react-bootstrap/Container';
+
 
 
 
 
 const App= () => {
   return (
-    <div>
+    <div className="App" style={{position:"relative"}}>
       <MyNavbar/>
       <MyCarausel/>
       <TitleMessage/>
+      {/* aboutme*/}
+      <div>
+      <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+          <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
+      {/* skills section*/}
+      <div>
+      <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+          <Container className="container-box rounded">
+              <Fade duration={500}>
+                <Skills />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
+     
       
           </div>
   );
