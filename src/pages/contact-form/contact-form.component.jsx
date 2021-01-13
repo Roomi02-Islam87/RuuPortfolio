7fe3190c326981bb3062702a4de2a97f9f1a36d0
roomi islam <roomiislam098@gmail.com>
2021-01-13 15:24:14 +0530

    new changes

--- src/pages/contact-form/contact-form.component.jsx
+++ src/pages/contact-form/contact-form.component.jsx
@@ -14,7 +14,7 @@ const ContactForm = () => {
           <Col className="d-flex justify-content-center flex-wrap">
             <div className="m-2">
               <a href="roomiislam098@gmail.com" target="_blank" rel="noopener noreferrer">
-                <Button variant="outline-danger" title="akjha96@gmail.com">
+                <Button variant="outline-danger" title="roomiislam098@gmail.com">
                   <i className="fas fa-envelope-square"></i> Email Me
                 </Button>
               </a>
@@ -35,7 +35,7 @@ const ContactForm = () => {
               </a>
             </div>
             <div className="m-2">
-              <a href="https://www.youtube.com/user/MrAkjha96/" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.youtube.com/watch?v=pxQyPsw3ro8" target="_blank" rel="noopener noreferrer">
                 <Button id="youtube-btn" variant="outline-info" title="Lets code together!">
                 <i class="fab fa-youtube"></i> Youtube
                 </Button>
--- src/pages/experience/experience.component.jsx
+++ src/pages/experience/experience.component.jsx
@@ -2,43 +2,43 @@ import React from 'react'
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Card from "react-bootstrap/Card";
-import L_ACCENTURE from "../../assets/img/experience/accenture-3.svg";
+import L_ACCENTURE from "../../assets/img/experience/sp.png";
 import Tilt from "react-tilt";
 import "./experience.style.css";
 
 const Experience = () => {
     return (
         <div id='experience'>
-            <h1 className="pt-3 text-center font-details-b pb-3">EXPERIENCE</h1>
+            <h1 className="pt-3 text-center font-details-b pb-3">INTERNSHIP</h1>
             <Jumbotron className="jumbostyle">
                 <Container className="">
                     <Tilt options={{max:25}}>
                     <Card>
                     <Card.Header as="h5" className="d-flex justify-content-center flex-wrap">
-                    <Card.Img variant="top" className="img-resize" src={L_ACCENTURE} alt="Accenture logo" />
+                    <Card.Img variant="top" className="img-resize" src={L_ACCENTURE} alt="sparrow Softech pvt ltd logo" />
                     </Card.Header>
                 <Card.Body className="d-flex justify-content-center flex-column">
                 <div>
-                  <Card.Title className="text-center">Associate Software Engineer</Card.Title>
+                  <Card.Title className="text-center">Web Developement</Card.Title>
                 </div>
                 <div>
                   <Card.Text className="text-center style">
                     <strong className="body-title-style ">Full Stack Developer</strong>
                     <br />
-                    <strong>Technology:</strong> React JS, DOTNET, C#, MS-SQL, Python
+                    <strong>Technology:</strong> HTML,Bootstrap,PHP,MYSQLI,Javascript
                     <br />
-                    <strong>Duration:</strong> June 2019 - Present
+                    <strong>Duration:</strong> 23rd September - 20th October
                     <br/>
                     <strong> Description </strong>
                     <ul className="text-left">
+                      <li><strong>Exposes</strong>me to real-world experience </li>
                       <li><strong>Developed &amp; enhanced</strong> multiple features with customizability option across web application.</li>
-                      <li><strong>Developed</strong> automation system to create SQL bulk query scripts that 
-                        increased efficiency by 80% and decreased working hours from 4 hours to 30 mins per task.
-                      </li>
-                      <li><strong>Provided</strong> application maintenance while working as `Production Support`. 
-                      </li>
+                      
+                      <li><strong>Gain</strong>the practical experience & it teaches me time-management . </li>
                       <li><strong>Performed</strong> CRUD operations on multiple databases to load/ remove data according 
                       to the business requirements.</li>
+                      <li>It encourages personal development, but also a greater understanding of self.</li>
+                      <li>And Most Important I got guidaince with our Senior Employee</li>
                       {/* <li><strong>Co-created</strong> React Document used as a guide for new developers.</li> */}
 
                     </ul>
